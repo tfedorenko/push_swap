359f3a45b8b77880f7b50a10550cff47b6b19379
Tetiana Fedorenko <tfedoren@c1s12d56.42wolfsburg.de>
2022-05-30 20:16:32 +0200

    s

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 18:59:33 by tfedoren          #+#    #+#             */
-/*   Updated: 2022/05/30 19:49:31 by tfedoren         ###   ########.fr       */
+/*   Updated: 2022/05/30 20:16:27 by tfedoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ typedef struct s_stack
 	struct s_stack	*next;
 
 }	t_stack;
-
+//Siemanko
 t_stack	*new_node(int data);
 int		is_empty(t_stack *root);
 void	print_stack(t_stack *head);
