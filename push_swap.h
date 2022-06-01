09754a71ac005ed2b32580d04846e011a6dad00d
Tetiana Fedorenko <ftania1609@gmail.com>
2022-06-01 21:50:53 +0200

    upd

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 18:59:33 by tfedoren          #+#    #+#             */
-/*   Updated: 2022/06/01 21:07:55 by tfedoren         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:49:06 by tfedoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ void	print_stack(t_stack *head);
 t_stack	*push_to_stack(t_stack *root, int data);
 void	rotate(t_stack **stack);
 void	swap(t_stack *stack);
-// void	push(t_stack **stack1, t_stack **stack2);
+int		pop(t_stack **root);
+void	push(t_stack **head, int data);
 void	sa(t_stack *stack);
 void	sb(t_stack *stack);
 void	ss(t_stack *stack1, t_stack *stack2);
@@ -38,7 +39,6 @@ void	rb(t_stack **stack);
 void	rr(t_stack **stack1, t_stack **stack2);
 void	pa(t_stack **stack_a, t_stack **stack_b);
 void	pb(t_stack **stack_b, t_stack **stack_a);
-int		pop(t_stack **root);
-void	push(t_stack **head, int data);
+
 
 #endif
--- push_swap_push.c
+++ push_swap_push.c
@@ -6,7 +6,7 @@
 /*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 19:42:32 by tfedoren          #+#    #+#             */
-/*   Updated: 2022/06/01 21:21:32 by tfedoren         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:50:30 by tfedoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,20 @@
 
 void	push(t_stack **head, int data)
 {
-	t_stack *tmp = *head;
-	if(!tmp)
+	t_stack	*tmp;
+
+	tmp = *head;
+	if (!tmp)
 	{
 		*head = new_node(data);
 		return ;
 	}
-	while(tmp->next != NULL)
+	while (tmp->next != NULL)
 	{
 		tmp = tmp->next;
 	}
 	tmp->next = new_node(data);
 }
-// void	push(t_stack **stack_a, t_stack **stack_b)
-// {
-// 	// t_stack	*temp;
-	
-// 	// temp = *stack_b;
-// 	// if (!(temp))
-// 	// {
-// 	// 	perror("Error thingy\n");
-// 	// 	return ;
-// 	// }
-// 	// *stack_a = *stack_b;
-// 	// *stack_b = (*stack_b)->next;
-// 	// (*stack_b)->next = temp;
-// }
 
 void	pa(t_stack **stack_a, t_stack **stack_b)
 {
@@ -50,7 +38,6 @@ void	pa(t_stack **stack_a, t_stack **stack_b)
 
 void	pb(t_stack **stack_b, t_stack **stack_a)
 {	
-
 	push(stack_b, pop(stack_a));
-	ft_printf("pb\n");	
+	ft_printf("pb\n");
 }
