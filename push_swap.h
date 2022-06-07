9d0599a395eb0ad1eab10ba0af6e72c939bea564
Tetiana Fedorenko <ftania1609@gmail.com>
2022-06-07 18:51:09 +0200

    check_input somehow works

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 18:59:33 by tfedoren          #+#    #+#             */
-/*   Updated: 2022/06/07 18:15:28 by tfedoren         ###   ########.fr       */
+/*   Updated: 2022/06/07 18:50:10 by tfedoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,32 +24,32 @@ typedef struct s_stack
 }	t_stack;
 
 # define INT_MAX 2147483647
-# define INT_MIN -2147483647
+# define INT_MIN -2147483648
 
-long int		ft_atol(char *str);
-int		check_input(int n, char **input);
-int		check_duplicates(int n, char **input);
-int		check_integers(int n, char **input);
-t_stack	*create_stack(int n, char **input);
-t_stack	*new_node(int data);
-int		is_empty(t_stack *root);
-void	print_stack(t_stack *head);
-t_stack	*push_to_stack(t_stack *root, int data);
-void	rotate(t_stack **stack);
-void	rotate_reverse(t_stack **stack);
-void	swap(t_stack *stack);
-int		pop(t_stack **root);
-void	push(t_stack **head, int data);
-void	sa(t_stack *stack);
-void	sb(t_stack *stack);
-void	ss(t_stack *stack1, t_stack *stack2);
-void	ra(t_stack **stack);
-void	rb(t_stack **stack);
-void	rr(t_stack **stack1, t_stack **stack2);
-void	pa(t_stack **stack_a, t_stack **stack_b);
-void	pb(t_stack **stack_b, t_stack **stack_a);
-void	rra(t_stack **stack);
-void	rrb(t_stack **stack);
-void	rrr(t_stack **stack1, t_stack **stack2);
+long int	ft_atol(char *str);
+int			check_input(int n, char **input);
+int			check_duplicates(int n, char **input);
+int			check_integers(int n, char **input);
+t_stack		*create_stack(int n, char **input);
+t_stack		*new_node(int data);
+int			is_empty(t_stack *root);
+void		print_stack(t_stack *head);
+t_stack		*push_to_stack(t_stack *root, int data);
+void		rotate(t_stack **stack);
+void		rotate_reverse(t_stack **stack);
+void		swap(t_stack *stack);
+int			pop(t_stack **root);
+void		push(t_stack **head, int data);
+void		sa(t_stack *stack);
+void		sb(t_stack *stack);
+void		ss(t_stack *stack1, t_stack *stack2);
+void		ra(t_stack **stack);
+void		rb(t_stack **stack);
+void		rr(t_stack **stack1, t_stack **stack2);
+void		pa(t_stack **stack_a, t_stack **stack_b);
+void		pb(t_stack **stack_b, t_stack **stack_a);
+void		rra(t_stack **stack);
+void		rrb(t_stack **stack);
+void		rrr(t_stack **stack1, t_stack **stack2);
 
 #endif
--- push_swap_input_check.c
+++ push_swap_input_check.c
@@ -6,7 +6,7 @@
 /*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 16:39:15 by tfedoren          #+#    #+#             */
-/*   Updated: 2022/06/07 18:12:36 by tfedoren         ###   ########.fr       */
+/*   Updated: 2022/06/07 18:48:30 by tfedoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,8 @@ int	check_duplicates(int n, char **input)
 {
 	int	i;
 	int	j;
-	// int	*ptr;
 
-	// ptr = (int *)malloc(sizeof(int) * n);
 	i = 1;
-	// while (input[i])
-	// {
-	// 	ptr[i] = ft_atoi(input[i]);
-	// 	i++;
-	// }
-	// i = 1;
 	while (i < n - 1)
 	{
 		j = i + 1;
@@ -79,23 +71,19 @@ int	check_max_integer(int n, char **input)
 	i = 1;
 	while (i < n)
 	{
-		printf("I'm here %ld\n", ft_atol(input[i]));
-		if (ft_atol(input[i]) > INT_MAX || ft_atol(input[i]) < INT_MIN)
+		if (ft_atol(input[i]) > INT_MAX || ft_atol(input[i]) <= INT_MIN)
 		{
-			printf("I'm here %ld\n", ft_atol(input[i]));
-			ft_printf("Error:some arguments are bigger or smaller than an integer");
+			ft_printf("Error:some arguments are bigger/smaller than an int");
 			return (-1);
 		}
-			i++;
+		i++;
 	}
 	return (0);
 }
 
 int	check_input(int n, char **input)
 {
-	// if (!check_integers(n, input) && !check_duplicates(n, input) && check_max_integer(n, input))
-		// return (0);
-	if (!check_max_integer(n, input))
+	if (!check_integers(n, input) && !check_duplicates(n, input) && !check_max_integer(n, input))
 		return (0);
 	else
 		return (-1);
