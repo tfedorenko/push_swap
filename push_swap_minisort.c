e079da0ddcba1bd5bdb3e4359f1019f466f2655d
Tetiana Fedorenko <ftania1609@gmail.com>
2022-06-10 23:10:42 +0200

    upd e

--- push_swap_minisort.c
+++ push_swap_minisort.c
@@ -6,7 +6,7 @@
 /*   By: tfedoren <tfedoren@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 15:08:00 by tfedoren          #+#    #+#             */
-/*   Updated: 2022/06/10 22:37:07 by tfedoren         ###   ########.fr       */
+/*   Updated: 2022/06/10 23:09:56 by tfedoren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,39 +40,129 @@ int	find_min(t_stack *stack)
 	return (min);
 }
 
-void	sort_four( t_stack **stack_a, t_stack **stack_b)
-{
-	if ((*stack_a)->next->next->next->data == find_max(*stack_a))
-		rra(stack_a);
-	if ((*stack_a)->next->next->next->data == find_min(*stack_a))
-		ra(stack_a);
-	pb(stack_b, stack_a);
-	while (check_if_sorted(stack_a) && stack_b != NULL)
-		push_swap_microsort(stack_a, stack_b);
-	if ((*stack_a)->data < (*stack_b)->data && \
-			(*stack_a)->next->data > (*stack_b)->data)
-		ra(stack_a);
-	else
-		rra(stack_a);
-	pa(stack_a, stack_b);
-	if (check_if_sorted(stack_a))
-		ra(stack_a);
-}
-
 void	push_swap_minisort(t_stack **stack_a, t_stack **stack_b)
 {
-	int	stack_size;
-
+	int stack_size;
+	
 	stack_size = (ft_stack_size(*stack_a));
-	if (ft_stack_size(*stack_a) == 4)
+	if(ft_stack_size(*stack_a) == 4)
 	{
-		sort_four(stack_a, stack_b);
+		if ((*stack_a)->next->next->next->data == find_max(*stack_a))
+			rra(stack_a);
+		if ((*stack_a)->next->next->next->data == find_min(*stack_a))
+			ra(stack_a);
+		pb(stack_b, stack_a);
+		while (check_if_sorted(stack_a) && stack_b != NULL)
+		{
+			push_swap_microsort(stack_a, stack_b);
+		}
+		if ((*stack_b)->data > find_max(*stack_a))
+		{
+			pa(stack_a, stack_b);
+			return ;
+		}
+		if ((*stack_b)->data < find_min(*stack_a))
+		{
+			pa(stack_a, stack_b);
+			rra(stack_a);
+			return ;
+		}
+		ft_printf("Stack_before_sort 5=: ");
+		print_stack(*stack_a);
+		if((*stack_b)->data > (*stack_a)->data)
+		{
+			if ((*stack_b)->data > (*stack_a)->data)
+			rra(stack_a);
+				ft_printf("Stack_after_rotate1: ");
+			print_stack(*stack_a);
+			pa(stack_a, stack_b);
+				ft_printf("Stack_after_pa: ");
+			print_stack(*stack_a);
+			ra(stack_a);
+			// ra(stack_a);
+		}
+		// if ((*stack_a)->data < (*stack_b)->data && (*stack_a)->next->data > (*stack_b)->data)
+		// {
+		// 	ra(stack_a);
+		// 	ft_printf("Stack_after_rotate: ");
+		// 	print_stack(*stack_a);
+
+		// }
+		// else
+		// {
+		// 	rra(stack_a);
+		// }
+
+		// pa(stack_a, stack_b);
+
+		// if (check_if_sorted(stack_a))
+		// {
+		// 	ra(stack_a);
+
+		// }
+
 	}
-	if (ft_stack_size(*stack_a) == 5)
+
+		
+	if(ft_stack_size(*stack_a) == 5)
 	{
 		pb(stack_b, stack_a);
 		pb(stack_b, stack_a);
 	}
+	// pb(stack_b, stack_a);
+	// ft_printf("Stack_a: ");
+	// print_stack(*stack_a);
+	// ft_printf("Stack_b: ");
+	// print_stack(*stack_b);
+	
+	// while (check_if_sorted(stack_a) && stack_b != NULL)
+	// {
+	// 	push_swap_microsort(stack_a, stack_b);
+	// 	ft_printf("After_microsort: ");
+	// 	print_stack(*stack_a);
+	// }
+	// if (stack_size == 4)
+	// {
+		// 	// ft_printf("Stack_before_sort 5=: ");
+		// 	// print_stack(*stack_a);
+		// if ((*stack_b)->data > find_max(*stack_a))
+		// {
+		// 	pa(stack_a, stack_b);
+		// 	return ;
+		// }
+		// if ((*stack_b)->data < find_min(*stack_a))
+		// {
+		// 	pa(stack_a, stack_b);
+		// 	rra(stack_a);
+		// 	return ;
+		// }
+		
+		// if ((*stack_a)->data < (*stack_b)->data && (*stack_a)->next->data > (*stack_b)->data)
+		// {
+		// 	ra(stack_a);
+		// 	ft_printf("Stack_after_rotate: ");
+		// 	print_stack(*stack_a);
+		// }
+		// else
+		// {
+		// 	rra(stack_a);
+		// 	ft_printf("Stack_after_reverse_rotate: ");
+		// 	print_stack(*stack_a);
+		// }
+		// ft_printf("Stack_before_insert: ");
+		// print_stack(*stack_a);
+		// pa(stack_a, stack_b);
+		// ft_printf("Stack_after_insert: ");
+		// print_stack(*stack_a);
+		// if (check_if_sorted(stack_a))
+		// {
+		// 	ra(stack_a);
+		// 	printf("I'm here5\n");
+		// }
+		
+	// ft_printf("Stack_a11: ");
+	// print_stack(*stack_a);
+	// }
 	return ;
 }
 // 1 2 3 4 5
@@ -81,3 +171,4 @@ void	push_swap_minisort(t_stack **stack_a, t_stack **stack_b)
 // 4 1 2 3 5
 // 5 1 2 3 4
 // 1 3 2 4 5
+
